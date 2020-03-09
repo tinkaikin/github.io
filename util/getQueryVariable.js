965631931d53ca57a05cd15b826515797b60c7b0
tinkaikin <tinkaikin@163.com>
2020-03-09 17:01:28 +0800

    121234

---
+++ util/getQueryVariable.js
@@ -0,0 +1,17 @@
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+/*
+http://www.runoob.com/index.php?id=1&image=awesome.jpg
+调用 getQueryVariable("id") 返回 1。
+
+调用 getQueryVariable("image") 返回 "awesome.jpg"。
+*/ 
